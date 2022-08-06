5eef9f19ab9479502df19b1f2e5598525264b861
Andrew A <tov_mayor@mail.ru>
2022-08-06 15:01:57 +0300

    working on sestatus - 1

--- sebash.sh
+++ sebash.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+#su permissions check
+
 if [ `id -u` != 0 ]
 then
     echo "Please run  as su"
     exit 1
 fi    
+
+#selinux status check
+
+for s in `sestatus`
+do
+    echo "$s ---"
+done
