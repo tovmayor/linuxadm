2f498121cccde8ec08059d0f93567ac0783ce847
Andrew A <tov_mayor@mail.ru>
2022-08-06 17:10:59 +0300

    3/4

--- sebash.sh
+++ sebash.sh
@@ -9,44 +9,65 @@ fi
 
 #selinux status check
 
-#ses=()
-
 IFS=$'\n'
-echo -en "SELinux working status: \n"
+echo -e "SELinux working status: \n"
 for s in `sestatus`
 do
     if [ $s = "Current mode:                   enforcing" ]
     then
-        echo -e "\033[32\tSELinux security policy is enforced"
+        echo -e "\tSELinux security policy is enforced"
     elif [ $s = "Current mode:                   permissive" ]
     then
-        echo -e "\033[33\tSELinux prints warnings instead of enforcing"
+        echo -e "\tSELinux prints warnings instead of enforcing"
     elif [ $s = "Current mode:                   disabled" ]
     then
-        echo -e "\033[31\tNo SELinux policy is loaded"
+        echo -e "\tNo SELinux policy is loaded"
 #    else 
-#        echo "Some shit"
+#        echo "Some shit happened"
     fi
 
 #    echo "$s ---"
 done
 
+#enable SELinux
+
+read -p "Do you want to enable(e) or disable(d) SELinux?" enabl
+if [ $enabl == "e" ]
+then 
+    setenforce 1
+elif [ $enabl == "d" ]
+then 
+    setenforce 0
+fi
+
+
 #parsing /etc/selinux/confug
-echo -en "SELinux config file activation status: \n"
+echo -e "SELinux config file activation status: \n"
 
 for c in `cat /etc/selinux/config`
 do
     if [ $c = "SELINUX=enforcing" ]
     then
-        echo -e "\033[32\tSELinux security policy is enforced"
+        echo -e "\tSELinux security policy is enforced"
     elif [ $c = "SELINUX=permissive" ]
     then
-        echo -e "\033[33\tSELinux prints warnings instead of enforcing"
+        echo -e "\tSELinux prints warnings instead of enforcing"
     elif [ $c = "SELINUX=disabled" ]
     then
-        echo -e "\033[31\tNo SELinux policy is loaded"
+        echo -e "\tNo SELinux policy is loaded"
 #    else 
 #        echo "Some shit"
     fi
 
 done
+
+#enable SELinux in config
+
+read -p "Do you want to enable(e) or disable(d) SELinux in config file?" conf
+if [ $conf == "e" ]
+then 
+    setenforce 1
+elif [ $conf == "d" ]
+then 
+    setenforce 0
+fi
