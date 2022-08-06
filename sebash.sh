4987bc26c1e01493d0bb22b5c3f851bd4427d124
Andrew A <tov_mayor@mail.ru>
2022-08-06 16:00:31 +0300

    colored output

--- sebash.sh
+++ sebash.sh
@@ -17,13 +17,13 @@ for s in `sestatus`
 do
     if [ $s = "Current mode:                   enforcing" ]
     then
-        echo -e "\tSELinux security policy is enforced"
+        echo -e "\033[32\tSELinux security policy is enforced"
     elif [ $s = "Current mode:                   permissive" ]
     then
-        echo -e "\tSELinux prints warnings instead of enforcing"
+        echo -e "\033[33\tSELinux prints warnings instead of enforcing"
     elif [ $s = "Current mode:                   disabled" ]
     then
-        echo -e "\tNo SELinux policy is loaded"
+        echo -e "\033[31\tNo SELinux policy is loaded"
 #    else 
 #        echo "Some shit"
     fi
@@ -38,13 +38,13 @@ for c in `cat /etc/selinux/config`
 do
     if [ $c = "SELINUX=enforcing" ]
     then
-        echo -e "\tSELinux security policy is enforced"
+        echo -e "\033[32\tSELinux security policy is enforced"
     elif [ $c = "SELINUX=permissive" ]
     then
-        echo -e "\tSELinux prints warnings instead of enforcing"
+        echo -e "\033[33\tSELinux prints warnings instead of enforcing"
     elif [ $c = "SELINUX=disabled" ]
     then
-        echo -e "\tNo SELinux policy is loaded"
+        echo -e "\033[31\tNo SELinux policy is loaded"
 #    else 
 #        echo "Some shit"
     fi
