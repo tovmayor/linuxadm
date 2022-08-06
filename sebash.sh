64b541b8c560b036337ad3a9d894583de63b5959
Andrew A <tov_mayor@mail.ru>
2022-08-06 15:51:53 +0300

    config parsing

--- sebash.sh
+++ sebash.sh
@@ -12,6 +12,7 @@ fi
 #ses=()
 
 IFS=$'\n'
+printf "SELinux working status: \n"
 for s in `sestatus`
 do
     if [ $s = "Current mode:                   enforcing" ]
@@ -23,9 +24,29 @@ do
     elif [ $s = "Current mode:                   disabled" ]
     then
         echo "No SELinux policy is loaded"
-    else 
-        echo "Some shit"
+#    else 
+#        echo "Some shit"
     fi
 
 #    echo "$s ---"
+done
+
+#parsing /etc/selinux/confug
+printf "SELinux config file activation status: \n"
+
+for c in `cat /etc/selinux/config`
+do
+    if [ $c = "SELINUX=enforcing" ]
+    then
+        echo "SELinux security policy is enforced"
+    elif [ $c = "SELINUX=permissive" ]
+    then
+        echo "SELinux prints warnings instead of enforcing"
+    elif [ $c = "SELINUX=disabled" ]
+    then
+        echo "No SELinux policy is loaded"
+#    else 
+#        echo "Some shit"
+    fi
+
 done
