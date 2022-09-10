c4980369054ed04732792ff88c57370f53959b5f
Andrew A <tov_mayor@mail.ru>
2022-09-10 14:40:18 +0300

    commit

--- mysql_ce_install.sh
+++ mysql_ce_install.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 rpm -Uvh https://repo.mysql.com/mysql80-community-release-el7-7.noarch.rpm
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
-yum --enablerepo=mysql80-community install mysql-community-server
+yum --enablerepo=mysql80-community install mysql-community-server -y
 systemctl start mysqld
 systemctl enable mysqld
-mysql_secure_installation
 grep "A temporary password" /var/log/mysqld.log
+mysql_secure_installation
