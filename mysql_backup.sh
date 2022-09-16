e1a7fa321d5aa25994644dab95b3f89ecb9d0908
Andrew A <tov_mayor@mail.ru>
2022-09-16 21:49:09 +0300

    backup script test

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,1 +1,10 @@
-fixed
+#!bin/bash
+user=repluser
+pwd=CoolPasswd@2022
+MYSQL='mysql --skip-column-names'
+
+for s in mysql '$MYSQL -u$user -p$pwd -e SHOW DATABASES';
+    do
+#        mkdir $s;
+        echo -e "$s\n";
+done
