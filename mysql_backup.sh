e7590d2b8306d65ed1466ba7a414d635d51ddf63
Andrew A <tov_mayor@mail.ru>
2022-09-16 22:51:32 +0300

    =mysql

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -10,11 +10,11 @@ if [ ! -d backup ];
         rm -rf ./backup/*
 fi
 
-for s in mysql `$MYSQL -u$user -p$pwd -e "SHOW DATABASES LIKE '%_db'"`;
+for s in `$MYSQL -u$user -p$pwd -e "SHOW DATABASES LIKE '%_db'"`;
     do
         mkdir ./backup/$s;
 #        echo -e "$s\n";
-        for t in mysql `$MYSQL -u$user -p$pwd -e "SHOW TABLES FROM $s"`;
+        for t in `$MYSQL -u$user -p$pwd -e "SHOW TABLES FROM $s"`;
             do
                 echo -e "$s.$t\n";
 
