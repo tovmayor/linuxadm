8249bd466be7cfa66f237e11b81d67e364e71099
Andrew A <tov_mayor@mail.ru>
2022-09-16 22:18:07 +0300

    tables

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -5,6 +5,12 @@ MYSQL='mysql --skip-column-names'
 
 for s in mysql `$MYSQL -u$user -p$pwd -e "SHOW DATABASES"`;
     do
-#        mkdir $s;
-        echo -e "$s\n";
+        mkdir $s;
+#        echo -e "$s\n";
+        for t in mysql `$MYSQL -u$user -p$pwd -e "SHOW TABLES FROM $s"`;
+            do
+                echo -e "$s.$t\n";
+
+        done
+
 done
