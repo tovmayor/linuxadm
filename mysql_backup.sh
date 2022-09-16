f7d9b832cdcdac7fe7104028438958bea2e291e9
Andrew A <tov_mayor@mail.ru>
2022-09-16 22:07:01 +0300

    ``

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-user=repluser
-pwd=CoolPasswd@2022
+user='repluser'
+pwd='CoolPasswd@2022'
 MYSQL='mysql --skip-column-names'
 
-for s in mysql '$MYSQL -u$user -p$pwd -e SHOW DATABASES';
+for s in mysql `$MYSQL -u$user -p$pwd -e SHOW DATABASES`;
     do
 #        mkdir $s;
         echo -e "$s\n";
