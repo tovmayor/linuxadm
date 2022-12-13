1f3c556ff10f9b191bd09d32dfd6bd0059827abc
tovmayor <tov_mayor@mail.ru>
2022-12-13 13:21:50 +0000

    for mariadd 10.3

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -5,23 +5,27 @@
 #password = "very complex"
 
 MYSQL='mysql --defaults-extra-file=/root/my_sql.cnf --skip-column-names'
-DUMP='mysqldump --defaults-extra-file=/root/my_sql.cnf --source-data=2'
+DUMP='mysqldump --defaults-extra-file=/root/my_sql.cnf --master-data'
+
+date=$(date '+%Y-%m-%d')
 
 if [ ! -d backup ]; 
     then 
         mkdir backup
+	mkdir backup/$date
     else 
-        rm -rf ./backup/*
+	mkdir backup/$date
+#        rm -rf ./backup/*
 fi
 
-for s in `$MYSQL -e "SHOW DATABASES LIKE '%_db'"`;
+for s in `$MYSQL -e "SHOW DATABASES LIKE '%wiki'"`;
     do
-        mkdir ./backup/$s;
+        mkdir ./backup/$date/$s;
 
         for t in `$MYSQL -e "SHOW TABLES FROM $s"`;
             do
                 echo -e "$s.$t\n";
-                $DUMP $s $t | gzip -3 > ./backup/$s/$s.$t.gz;
+                $DUMP $s $t | gzip -3 > ./backup/$date/$s/$s.$t.gz;
         done
 
 done
