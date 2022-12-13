bd24c8b905fdaef5074b7027066d325c4e335972
tovmayor <tov_mayor@mail.ru>
2022-12-13 16:15:01 +0300

    mysql8 copy

---
+++ mysql_backup mysql8.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# adding client config file  with mesql username & password /home/admin/my_sql.cnf like:
+#[client]
+#user = "some"
+#password = "very complex"
+
+MYSQL='mysql --defaults-extra-file=/root/my_sql.cnf --skip-column-names'
+DUMP='mysqldump --defaults-extra-file=/root/my_sql.cnf --source-data=2'
+
+if [ ! -d backup ]; 
+    then 
+        mkdir backup
+    else 
+        rm -rf ./backup/*
+fi
+
+for s in `$MYSQL -e "SHOW DATABASES LIKE '%_db'"`;
+    do
+        mkdir ./backup/$s;
+
+        for t in `$MYSQL -e "SHOW TABLES FROM $s"`;
+            do
+                echo -e "$s.$t\n";
+                $DUMP $s $t | gzip -3 > ./backup/$s/$s.$t.gz;
+        done
+
+done
